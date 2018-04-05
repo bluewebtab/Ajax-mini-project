3e8fda857afbe776136837827e5e67dc116927e6
Ronny Gutierrez <ronny.icne@gmail.com>
2018-04-04 19:36:10 -0700

    update 7:35pm

--- index.js
+++ index.js
@@ -5,7 +5,21 @@ function getProfile(){
     xhttp.onreadystatechange = function(){
         if(xhttp.readyState = 4 && xhttp.status == 200){
             var user = JSON.parse(xhttp.responseText);
-            document.getElementById('profile').innerHTML = user.name;
+            document.getElementById('profile').innerHTML =  `<div class="panel panel-default">
+            <div class="panel-heading">
+            <h3 class="panel-title">${user.name}</h3>
+            </div>
+             <div class="panel-body">
+              <div class="row">
+              <div class="col-md-3">
+              <img src = "${user.avatar_url}"
+              </div>
+              <div class="col-md-9>
+
+             </div>
+             </div>
+             </div>
+             </div>`;
         }
     }
     xhttp.open('GET', 'https://api.github.com/users/' + username, true);
